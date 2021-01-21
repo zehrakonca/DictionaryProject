0a85685db04d6364d5afc768e18f6fc672087681
Zehra KONCA <zehrakonca09@gmail.com>
2021-01-21 14:09:08 +0300

    dictionary

---
+++ MyDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryProject
+{
+    class MyDictionary<Tkey, TValue>
+    {
+        Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+        public void Add(int key, string value)
+        {
+
+        }
+        public MyDictionary()
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace DictionaryProject
     {
         static void Main(string[] args)
         {
+            Dictionary<int, string> hogwartsDatabase = new Dictionary<int, string>();
+            hogwartsDatabase.Add(56, "Rubeus Hagrid");
+            hogwartsDatabase.Add(16, "Filch's Cat");
+
+            Dictionary<int, string>.KeyCollection KeyList = hogwartsDatabase.Keys;
+
+            foreach (KeyValuePair<int,string> item in hogwartsDatabase)
+            {
+                Console.WriteLine("Number: {0} , Name: {1}", item.Key, item.Value);
+            }
+            Console.ReadLine();
         }
     }
 }
